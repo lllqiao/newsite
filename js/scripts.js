6219996be7c9874892b78d95bb2ce5c29617a77a
chan031777 <a109070059@mail.shu.edu.tw>
2021-05-21 15:58:05 +0800

    521

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,51 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+<!DOCTYPE html>
+<html>
+<head>
+<tittle>捷運美食</tittle>
+</head>
+<body>
+      <h3>關於我們</h3>
+	  <img src="照片名字"></img>
+	  <img src="照片名字"></img>
+	  <img src="照片名字"></img>
+	  <h3>創作理念</h3>
+	  <h3>捷運介紹</h3>
+	   <ul>
+	     <li>方案與設施</li>
+		 <li><a href="https://www.bnext.com.tw/article/56322/mrt-new-fee-2020">參考資料</a></li>
+	  </ul>
+	  <h3>國家</h3>
+	  <table border="1" width="400" cellpadding="5">
+	  <tr>
+	     <td>亞洲</td>
+		 <td>印尼</td>
+		 <td>越南</td>
+	     <td>泰國</td>
+		 <td>馬來西亞</td>
+	  </tr>
+	  <tr>
+	     <td>美洲</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+	  </tr>
+	  	  <tr>
+	     <td>歐洲</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+		 <td>國家名字</td>
+	  </tr>
+	   <ul>
+	     <li>美食</li>
+		 <li>交通</li>
+		 
+	  </ul>
+	  <h3>總結</h3>
+</body>
+</html>
